e826436f6024e864aff08b3530f0ee3ee7c6f43d
Nejc Konjiček <mrnejco@gmail.com>
2023-01-12 12:47:44 +0100

    changed text above textfield

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times, because you are very cool :) :',
+              'Vpisi stevilo ce si kul :)',
             ),
             TextField(
               controller: txt_controller,
