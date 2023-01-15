5644349f99ad0bc2c6256b7c4f0d8a7df2f98ce4
Rene Potocnik <rene.potocnik1@gmail.com>
2023-01-15 20:13:28 +0100

    Hide AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -60,11 +60,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   title: Text(widget.title),
+      // ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
