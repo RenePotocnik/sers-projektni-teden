4cbc410c5ff309263dd1394038998dbb6f6ef8fa
Rene Potocnik <rene.potocnik1@gmail.com>
2023-01-15 18:02:24 +0100

    Change app theme to system default

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Projektni Teden',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'SERS Projektni Teden'),
+      // Set the theme to the system default; `theme` or `darkTheme`
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+
+      home: const MyHomePage(title: 'SERS ECO - Projektni Teden',),
     );
   }
 }
